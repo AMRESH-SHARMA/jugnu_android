919717ae9f35e6eeb5289ae192729c2c622aa0a2
roshaniShirsat <roshani16300shirsat@gmail.com>
2025-12-25 23:25:52 +0530

    Get All Listeners paging

--- app/src/main/java/com/example/app/feature/listeners/data/ListenerRepository.kt
+++ app/src/main/java/com/example/app/feature/listeners/data/ListenerRepository.kt
@@ -8,9 +8,24 @@ import javax.inject.Inject
 class ListenerRepository @Inject constructor(
     private val api: ListenerApi
 ) {
-    suspend fun getListeners(): ApiResult<List<ListenerModel>> = safeApiCall {
-        val res = api.getListeners()
+    suspend fun getListeners(
+        page: Int,
+        limit: Int
+    ): ApiResult<Pair<List<ListenerModel>, Int>> = safeApiCall {
+
+        val res = api.getListeners(page, limit)
         if (!res.success) throw Exception(res.message)
-        res.data.map { it.toDomain() }
+
+        val listeners = res.data.map { it.toDomain() }
+
+        val total = (res.meta?.get("total") as? Number)?.toInt()
+            ?: listeners.size
+
+        listeners to total
     }
+//    suspend fun getListeners(): ApiResult<List<ListenerModel>> = safeApiCall {
+//        val res = api.getListeners()
+//        if (!res.success) throw Exception(res.message)
+//        res.data.map { it.toDomain() }
+//    }
 }
